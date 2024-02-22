c88fb53d06c77683c95c06d4ef69ef5a98cbefed
nickAmos <132029265+nickAmos@users.noreply.github.com>
2024-02-23 08:47:25 +1100

    About Me
    Added text for about me, setting up containers for animation.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   useGSAP(() => {
     gsap.from(".test-text", {
       y: () => document.querySelector(".background").offsetHeight,
-      duration: 5
+      duration: 1
     })
   });
 
@@ -25,11 +25,24 @@ function App() {
         className="progress-bar"
         style={{ scaleX: scrollYProgress }}
       />
-     
+
+
+     <div id='AboutME-Container'>
       <div className='test-text' id='test-text'>
-        <h1>Hi, I'm Nick!</h1>
-        <p>About me text looks good in a 300 font weight with a smaller font size</p>
+        <h1>Hi, my name is Nick !</h1>
+        <p>I'm a <span>JavaScript</span> / <span>React</span> developer who enjoys making interactive sites for the web.
+          I started learning to code in October 2022 with Codecademy's frontend-engineer course. 
+           Currently, I work as a laboratory assistant in a pathology lab. For fun I enjoy bloudering 
+           and playing sports with friends. 
+        </p>
+        <h2>Contact me</h2>
       </div>
+      <div id='img-container'>
+
+      </div>
+      </div>
+
+
      
     </div>
   );
