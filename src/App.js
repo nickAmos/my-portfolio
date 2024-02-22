bec370ff437b2e6b0c1a62dfdf8bb24601c19c1e
nickAmos <132029265+nickAmos@users.noreply.github.com>
2024-02-23 08:10:08 +1100

    Style: colour and font

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { motion, useScroll} from "framer-motion";
+
+import { gsap } from "gsap";
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+
 function App() {
+
+  const { scrollYProgress } = useScroll();
+
+  useGSAP(() => {
+    gsap.from(".test-text", {
+      y: () => document.querySelector(".background").offsetHeight,
+      duration: 5
+    })
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='background' id='App-Background'>
+      <motion.div
+        className="progress-bar"
+        style={{ scaleX: scrollYProgress }}
+      />
+     
+      <div className='test-text' id='test-text'>
+        <h1>Hi, I'm Nick!</h1>
+        <p>About me text looks good in a 300 font weight with a smaller font size</p>
+      </div>
+     
     </div>
   );
 }
