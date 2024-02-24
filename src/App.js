a774823cbaeecbc6c009a31ba7396b7f52a4d667
nickAmos <132029265+nickAmos@users.noreply.github.com>
2024-02-24 16:40:45 +1100

    Refact: starting animation size

--- src/App.js
+++ src/App.js
@@ -6,17 +6,42 @@ import { gsap } from "gsap";
 import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
 
   const { scrollYProgress } = useScroll();
 
   useGSAP(() => {
-    gsap.from(".test-text", {
-      y: () => document.querySelector(".background").offsetHeight,
-      duration: 1
+
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".aboutME",
+        start: "center center",
+        end: "+=1000",
+        scrub: true,
+        markers: true,
+        pin: true
+      }
     })
+
+    tl.to(".aboutME",{
+    x: 0});   
+
+    gsap.from(".test", {
+      scrollTrigger: {
+        trigger: '.test',
+        start: 'top center',
+        end: '+=600',
+        scrub: 1,
+        markers: true
+      },
+      opacity: 0,
+      x: 200
+    })
+
   });
+  
 
 
   return (
@@ -25,21 +50,24 @@ function App() {
         className="progress-bar"
         style={{ scaleX: scrollYProgress }}
       />
+      <div id='top-screen-box'> 
 
 
-     <div id='AboutME-Container'>
-      <div className='test-text' id='test-text'>
-        <h1>Hi, my name is Nick !</h1>
-        <p>I'm a <span>JavaScript</span> / <span>React</span> developer who enjoys making interactive sites for the web.
+      <div className='aboutME' id='AboutME-Container'>
+        <div className='test' id='test-text'>
+         <h1>Hi, my name is Nick !</h1>
+         <p>I'm a <span>JavaScript</span> / <span>React</span> developer who enjoys making interactive sites for the web.
           I started learning to code in October 2022 with Codecademy's frontend-engineer course. 
            Currently, I work as a laboratory assistant in a pathology lab. For fun I enjoy bloudering 
            and playing sports with friends. 
-        </p>
-        <h2>Contact me</h2>
-      </div>
-      <div id='img-container'>
+         </p>
+         <h2>Contact me</h2>
+        </div>
+        <div id='img-container'>
 
-      </div>
+        </div>
+        </div>
+
       </div>
 
 
