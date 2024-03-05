28b1df420351e0be5fd45e3f5b5cc3f98609eb4c
nickAmos <132029265+nickAmos@users.noreply.github.com>
2024-03-06 09:27:44 +1100

    Style: scroll Trigger
    Animated instruction to scroll upon landing on the first page.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ import { gsap } from "gsap";
 import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Techstack from './Components/Techstack';
-import { Icon } from 'semantic-ui-react';
-import { useState } from 'react';
 import Projects from './Components/Projects';
 import Contact from './Components/Contact';
+import { Icon } from 'semantic-ui-react';
 
 
 gsap.registerPlugin(ScrollTrigger);
@@ -21,10 +20,46 @@ function App() {
 
   useGSAP(() => {
 
+
+    let scroll = gsap.timeline({
+      duration: 1
+    });
+
+    scroll.from(".scroll-txt", {
+      opacity:0,
+      y: -30,
+      duration:1,
+      ease: 'expo.out'
+    });
+
+    scroll.from(".scroll-icon", {
+      opacity: 0,
+      y: -10,
+      duration: 0.5
+    }, '-=0.5');
+
+    let scrollExit = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'background',
+        start: "10 top",
+        end: '20 top',
+        markers: true
+      }
+    });
+
+    scrollExit.to(".scroll-instruction-container", {
+      opacity:0
+    })
+
+
+
+
+
+
     let intro = gsap.timeline({
       scrollTrigger: {
         trigger: '.background',
-        start: "10 top",
+        start: "50 top",
         end: "+=150",
         //markers: true
       },
@@ -52,34 +87,7 @@ function App() {
       opacity:0,
       duration: 2,
       ease: 'power3'
-    }, '-=2')
-/* 
-    gsap.from(".p", {
-      scrollTrigger: {
-        trigger: '.background',
-        start: '150 top',
-        end: "+=150",
-        //markers: true
-      },
-      width: 1200,
-      duration: 2,
-      ease: 'power3'
-    })
-
-    gsap.from(".image", {
-      scrollTrigger: {
-        trigger: '.background',
-        start: '150 top',
-        end: "+=150",
-        //markers: true
-      },
-      x: 400,
-      opacity:0,
-      duration: 2,
-      ease: 'power3'
-    })
-
-    */
+    }, '-=2');
 
     let tl = gsap.timeline({
       scrollTrigger: {
@@ -134,6 +142,13 @@ function App() {
         style={{ scaleX: scrollYProgress }}
       />
       <div id='top-screen-box'> 
+      <div id='scroll-instruction-container' className='scroll-instruction-container'>
+
+        <div id='scroll-icon' className='scroll-icon'><Icon size='big' name='angle double down' /></div>
+        <div id='scroll-txt' className='scroll-txt'>Scroll to begin</div>
+        <div id='scroll-icon' className='scroll-icon'><Icon size='big' name='angle double down' /></div>
+        
+      </div>
 
 
       <div className='aboutME' id='AboutME-Container'>
